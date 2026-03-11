6f63bd12e9b36e0f2da8b24b317042f6f284c587
left-insurance <anayparameshwaran14@gmail.com>
2026-03-11 17:45:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const product=document.getElementById("searchInput").value;
 alert("Searching prices for: "+product);
 
 }
+
+
+const toggle=document.getElementById("darkToggle");
+
+toggle.addEventListener("click",function(){
+
+document.body.classList.toggle("dark-mode");
+
+});
