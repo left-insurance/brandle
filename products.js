01b0fa3ff78552ce3e0e09c531db8dad1ccd4d41
left-insurance <anayparameshwaran14@gmail.com>
2026-03-13 20:35:59 +0530

    Update products.js

--- products.js
+++ products.js
@@ -3,52 +3,86 @@ const category = params.get("cat");
 
 const title = document.getElementById("categoryTitle");
 const grid = document.getElementById("productGrid");
+const filter = document.getElementById("priceFilter");
+
+let currentProducts = [];
 
 const products = {
 
 cool: [
-{ name:"Mini Projector", price:"₹2999" },
-{ name:"Magnetic Charging Cable", price:"₹399" },
-{ name:"Portable Monitor", price:"₹7999" }
+{ name:"Mini Projector", price:2999 },
+{ name:"Magnetic Charging Cable", price:399 },
+{ name:"Portable Monitor", price:7999 }
 ],
 
 toys: [
-{ name:"VR Headset", price:"₹2499" },
-{ name:"Mini Drone", price:"₹3499" },
-{ name:"Smart LED Cube", price:"₹999" }
+{ name:"VR Headset", price:2499 },
+{ name:"Mini Drone", price:3499 },
+{ name:"Smart LED Cube", price:999 }
 ],
 
 500: [
-{ name:"LED Strip Lights", price:"₹299" },
-{ name:"Phone Stand", price:"₹199" },
-{ name:"Cable Organizer", price:"₹149" }
+{ name:"LED Strip Lights", price:299 },
+{ name:"Phone Stand", price:199 },
+{ name:"Cable Organizer", price:149 }
 ],
 
 1000: [
-{ name:"Bluetooth Speaker", price:"₹899" },
-{ name:"Gaming Mouse", price:"₹799" },
-{ name:"Laptop Cooling Pad", price:"₹999" }
+{ name:"Bluetooth Speaker", price:899 },
+{ name:"Gaming Mouse", price:799 },
+{ name:"Laptop Cooling Pad", price:999 }
 ],
 
 trending: [
-{ name:"Smart Ring", price:"₹6999" },
-{ name:"AI Voice Recorder", price:"₹4999" },
-{ name:"Portable Projector", price:"₹3999" }
+{ name:"Smart Ring", price:6999 },
+{ name:"AI Voice Recorder", price:4999 },
+{ name:"Portable Projector", price:3999 }
 ]
 
 };
 
 title.innerText = category.toUpperCase() + " GADGETS";
 
-products[category].forEach(product => {
+currentProducts = [...products[category]];
 
-const card = document.createElement("div");
-card.className = "card";
+function displayProducts(list){
 
-card.innerHTML =
-"<h3>" + product.name + "</h3>" +
-"<p style='padding:15px'>Price: " + product.price + "</p>";
+grid.innerHTML="";
+
+list.forEach(product=>{
+
+const card=document.createElement("div");
+
+card.className="card";
+
+card.innerHTML=
+"<h3>"+product.name+"</h3>"+
+"<p style='padding:15px'>Price: ₹"+product.price+"</p>";
 
 grid.appendChild(card);
+
+});
+
+}
+
+displayProducts(currentProducts);
+
+filter.addEventListener("change",function(){
+
+let sorted=[...currentProducts];
+
+if(this.value==="low"){
+
+sorted.sort((a,b)=>a.price-b.price);
+
+}
+
+if(this.value==="high"){
+
+sorted.sort((a,b)=>b.price-a.price);
+
+}
+
+displayProducts(sorted);
 
 });
